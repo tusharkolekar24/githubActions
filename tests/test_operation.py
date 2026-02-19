753d71d17689e97293f4a7fafe79e746a21a758b
KolekarTushar <kolekartushar24041996@gmail.com>
2026-02-19 15:06:23 +0530

    created backup of dev enviroment

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -11,11 +11,7 @@ def test_add():
 def test_sub():
     assert sub(5,3)==2
     assert sub(4,3)==1
-    # assert sub(3,3)==0
+    assert sub(3,3)==0
     # assert sub(2,3)==-1
     # assert sub(5,3)==2
-    # assert sub(6,3)==3
-
-
-
-
+    assert sub(6,3)==3

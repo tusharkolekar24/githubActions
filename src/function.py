e89ce522f3db38d718a513e5ee4b82a6493ac820
KolekarTushar <kolekartushar24041996@gmail.com>
2026-02-19 18:44:55 +0530

    SQL update

---
+++ src/function.py
@@ -0,0 +1,57 @@
+import requests
+import json
+
+def send_data_to_apex(payload):
+    url = "https://oracleapex.com/ords/wksp_myapi/filedetail/insertdata"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "application/json"
+    }
+    
+    status_messages = "Fail to send data to Oracle APEX"
+    try:
+        # 🔹 Send POST request
+        response = requests.post(
+            url,
+            headers=headers,
+            data=json.dumps(payload)
+        )
+
+        # 🔹 Print results
+        # print("Status Code:", response.status_code)
+        # print("Response Text:", response.text)
+        status_messages = "Successfully sent data to Oracle APEX"
+    except Exception as e:
+        print("An error occurred:", str(e))
+
+    return status_messages
+
+def sql_payload(metadata):
+    payload = {
+      "cni": metadata['cni'],
+      "file_name": metadata['filename'],
+      "file_subfolder": metadata['file_subfolder'],
+      "file_extension": metadata['filetype'],
+      "message":metadata['filename'],
+    }
+    return payload
+
+def extract_metadata(file_path,messages):
+    
+    cni = int(messages.split(" ")[0].lower().replace("cni",""))
+    file_subfolder = file_path.split("/")[0]
+    file_extension = '.'+file_path.split("/")[-1].split(".")[-1]
+    filename       = file_path.split("/")[-1]
+    
+    # 🔹 Create metadata dictionary
+    metadata = {
+        "cni": cni,
+        "file_name": filename,
+        "file_subfolder": file_subfolder,
+        "file_extension": file_extension,
+        "message":messages
+         
+    }
+    
+    return metadata
--- tests/validation_test.py
+++ tests/validation_test.py
@@ -2,6 +2,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from src.function import extract_metadata, sql_payload, send_data_to_apex
 def validate_txt(file_path, message):
     print(f"Checking TXT file: {file_path}")
     print(f"Expected message: {message}")
@@ -12,7 +13,15 @@ def main():
         sys.exit(1)
 
     file_path = sys.argv[1]
-    message = sys.argv[2]
+    message   = sys.argv[2]
     validate_txt(file_path, message)
+    status = "Fail to send data to Oracle APEX"
+    if "cni" in message.lower():
+        metadata = extract_metadata(file_path, message)
+        payload = sql_payload(metadata)
+        status = send_data_to_apex(payload)
+        print(status)
+    print("SQL Payload Status:{}".format(status))
+    
 if __name__ == "__main__":
     main()
